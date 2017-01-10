436a2cc30e75583a78abcf5f270095d45dce662a
Mark Krawczuk <mkrawczuk@tw-mbp-mkrawczuk.attlocal.net>
2017-01-09 17:47:52 -0800

    Connected view controller items to swift object.

---
+++ MKCollector/MKViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MKViewController.swift
+//  MKCollector
+//
+//  Created by Mark Krawczuk on 1/9/17.
+//  Copyright © 2017 Mark Krawczuk. All rights reserved.
+//
+
+import UIKit
+
+class MKViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var MKImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
